6b23d26b3c7e13c4e6d3a72c084087c9164f6ff8
prabir18 <prabirsingh1804@gmail.com>
2026-04-22 23:12:05 -0400

    queue works

--- main/main.c
+++ main/main.c
@@ -1,7 +1,39 @@
 #include <stdio.h>
 #include "driver/gpio.h"
+#include "esp_attr.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/queue.h"
 
-void app_main(void)
-{
+#define MOTION_SENSOR_PIN 13
 
+static QueueHandle_t gpio_evt_queue = NULL;
+
+static void IRAM_ATTR isr_handler(void* arg) {
+    uint32_t pin_num = (uint32_t) arg;
+    xQueueSendFromISR(gpio_evt_queue, &pin_num, NULL);
+}
+
+void app_main(void) {
+    // config
+    gpio_config_t io_conf = {
+        .intr_type = GPIO_INTR_POSEDGE,
+        .mode = GPIO_MODE_INPUT,
+        .pin_bit_mask = (1ULL << MOTION_SENSOR_PIN),
+        .pull_down_en = 1,
+        .pull_up_en = 0
+    };
+
+    gpio_config(&io_conf);
+    gpio_install_isr_service(0); // installs gpio interrupt driver
+    gpio_isr_handler_add(MOTION_SENSOR_PIN, isr_handler, (void*) MOTION_SENSOR_PIN); // hooks pin to specific isr function
+    gpio_evt_queue = xQueueCreate(10, sizeof(uint32_t));
+
+    printf("Waiting for Motion \n");
+
+    uint32_t received_pin;
+    while(1) {
+        if (xQueueReceive(gpio_evt_queue, &received_pin, portMAX_DELAY)) {
+            printf("Motion detected, interrupt on pin %lu \n", received_pin);
+        }
+    }
 }
